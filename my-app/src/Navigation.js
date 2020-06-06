4158db060dc32af73801e44054365e760a8540ac
Utkarsh Chirimar <utkarsh.chirimar@gmail.com>
2020-06-06 14:10:00 -0400

    changes to nav bar

--- my-app/src/Navigation.js
+++ my-app/src/Navigation.js
@@ -1,32 +1,31 @@
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from './logo.png';
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 
 const navigation = () => {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home"><img src={logo} className="App-logo" alt="logo" /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#charity">Charity</Nav.Link>
+                    <NavDropdown title="Current Year" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/4.1">Board</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/4.2">Teams</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/4.3">Applications</NavDropdown.Item>
                     </NavDropdown>
+                    <NavDropdown title="Previous Years" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/5.1">ECS 3.0</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/5.2">ECS 2.0</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/5.3">ECS 1.0</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#tickets">Ticket Sales</Nav.Link>
                 </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
             </Navbar.Collapse>
         </Navbar>
     );
